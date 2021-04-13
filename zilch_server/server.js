7252bbcd68459adf9d7fab79b0b1632527d9738d
Maetran <manuel.henn@me.com>
2021-04-13 12:33:09 +0200

    D

--- zilch_server/server.js
+++ zilch_server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+const allUsers = {};
 
 express.static('./public');
 app.use(express.static('./public'));
@@ -10,6 +11,10 @@ app.use(express.static('./public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/zilch.html');
 });
+
+io.on('registerPlayer', (msg) => {
+  
+})
 
 server.listen(3003, () => {
     console.log('listening on *:3003');
